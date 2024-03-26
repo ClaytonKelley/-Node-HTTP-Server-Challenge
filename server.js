bffde6d7bc2fbbfe12e6db0d4b308b8803f827a8
Clayton <clayton.c.kelley2@gmail.com>
2024-03-27 01:30:02 +0900

    Prolly cheated but 5 lines with server running

---
+++ server.js
@@ -0,0 +1,5 @@
+const http = require('http');
+const HOST = 'http://localhost/'
+const PORT = 3000
+const server = http.createServer((request, response) => { response.statusCode = 200; response.end("Hello, Galvanize!");});
+server.listen(PORT, "localhost", () => console.log(`Server running at ${HOST}:${PORT}/`));
