4d2cc98448fb1be3ccfa670889d4e7c44916eff7
Clayton <clayton.c.kelley2@gmail.com>
2024-03-27 01:33:24 +0900

    down to 3 lines with out proper foramtting

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-const http = require('http');
-const HOST = 'http://localhost/'
-const PORT = 3000
+const http = require('http'); const HOST = 'http://localhost/' ; const PORT = 3000
 const server = http.createServer((request, response) => { response.statusCode = 200; response.end("Hello, Galvanize!");});
 server.listen(PORT, "localhost", () => console.log(`Server running at ${HOST}:${PORT}/`));
